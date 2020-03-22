b9cb701e6a88551c301a33b90b3c654395ea6824
BigWave8 <rostykchess@gmail.com>
2020-03-23 00:11:04 +0200

    added y/m/d format

--- src/main/java/ua/lviv/iot/lab6/Date.java
+++ src/main/java/ua/lviv/iot/lab6/Date.java
@@ -9,18 +9,20 @@ public class Date {
 
         String result = "";
 
-        Pattern patternOfDate = Pattern.compile("\\b(0?[1-9]|[12]\\d|3[01])\\/((0?[13-9])|(1[012]))\\/\\d+\\b");
+        Pattern patternOfDate = Pattern.compile(
+                "(\\b(0?[1-9]|[12]\\d|3[01])\\/((0?[13-9])|(1[012]))\\/\\d+\\b)|(\\b\\d+\\/((0?[13-9])|(1[012]))\\/(0?[1-9]|[12]\\d|3[01])\\b)");
         Matcher matcher = patternOfDate.matcher(textCheckDate);
         while (matcher.find()) {
             result += matcher.group();
         }
 
-        Pattern patternOfFebruary = Pattern.compile("\\b(0?[1-9]|[12]\\d)\\/2\\/\\d+\\b");
+        Pattern patternOfFebruary = Pattern
+                .compile("(\\b(0?[1-9]|[12]\\d)\\/2\\/\\d+\\b)|(\\b\\d+\\/2\\/(0?[1-9]|[12]\\d)\\b)");
         Matcher matcherOfFebruary = patternOfFebruary.matcher(textCheckDate);
 
         while (matcherOfFebruary.find()) {
             Pattern patternOfFebruaryIntercalary = Pattern.compile(
-                    "\\b29\\/2\\/(((([13579][01345789])|([02468][1235679]))00)|(\\d*(([02468][1235679])|([13579][01345789]))))\\b");
+                    "(\\b29\\/2\\/(((([13579][01345789])|([02468][1235679]))00)|(\\d{2}(([02468][1235679])|([13579][01345789]))))\\b)|(\\b(((([13579][01345789])|([02468][1235679]))00)|(\\d{2}(([02468][1235679])|([13579][01345789]))))\\/2\\/29\\b)");
             Matcher matcherOfFebruaryIntercalary = patternOfFebruaryIntercalary.matcher(matcherOfFebruary.group());
             if (matcherOfFebruaryIntercalary.find() == false) {
                 result += matcherOfFebruary.group();
--- src/test/java/ua/lviv/iot/lab6/TestDate.java
+++ src/test/java/ua/lviv/iot/lab6/TestDate.java
@@ -13,10 +13,11 @@ class TestDate {
     @Test
     public void testOfDate() {
 
-        assertEquals("14/1/2020" + "28/2/1200" + "28/2/1234" + "29/2/2000", date
-                .runPatterns("14/1/2020 in da hause BC3234BM 28/2/1200 28/2/1234 29/2/2000 29/2/2001 drunk as dog"));
-        assertEquals("29/2/33220", date.runPatterns("29/2/33220 riding"));
-        assertEquals("11/11/2020" + "1/11/2020", date.runPatterns("11/11/2020 00/11/2020 1/11/2020 BC3234BM riding"));
+        assertEquals("14/1/2020" + "1/1/1" + "28/2/1200" + "28/2/1234" + "29/2/2000",
+                date.runPatterns("14/1/2020 28/2/1200 28/2/1234 29/2/2000 29/2/2001 123 12312312 12/1111/111 1/1/1"));
+        assertEquals("29/2/33220", date.runPatterns("29/2/33220 30/2/33333 30/2/33320"));
+        assertEquals("11/11/2020" + "1/11/2020" + "2020/11/1" + "2222/1/11",
+                date.runPatterns("11/11/2020 00/11/2020 1/11/2020 2020/11/1 2222/1/11"));
     }
 
     @Test
